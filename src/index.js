de43784b6c1972c7e6bb5083d822039220d72595
luna <lunaceee@gmail.com>
2018-05-01 22:34:07 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import "file-loader?name=index.html!./index.html";
 import GoTrue from "gotrue-js";
 
 const auth = new GoTrue({
-  APIUrl: "https://imorente-gotrue-examples.netlify.com/.netlify/identity"
+  APIUrl: "https://kaldi-identity-access-test.netlify.com/.netlify/identity"
 });
 
 window.auth = auth;
 
+//sign up
 document.querySelector("form[name='signup']").addEventListener("submit", e => {
   e.preventDefault();
   const form = e.target;
@@ -17,6 +18,35 @@ document.querySelector("form[name='signup']").addEventListener("submit", e => {
       showMessage("Success! Response: " + JSON.stringify(response), form)
     )
     .catch(error => showMessage("Failed :( " + JSON.stringify(error), form));
+});
+
+//log in
+document.querySelector("form[name='login']").addEventListener("submit", e => {
+  e.preventDefault();
+  const form = e.target;
+  const { email, password } = form.elements;
+  auth
+    .login(email.value, password.value)
+    .then(response =>
+      showMessage("Success! Response: " + JSON.stringify(response), form)
+    )
+    .catch(error => showMessage("Failed :( " + JSON.stringify(error), form));
+});
+
+//reset password
+document.querySelector("form[name='resetpw']").addEventListener("submit", e => {
+  e.preventDefault();
+  const form = e.target;
+  const email = "luna+03@netlify.com";
+  auth
+    .requestPasswordRecovery(email)
+    .then(response =>
+      showMessage(
+        "Success!Recovery email sent" + JSON.stringify(response),
+        form
+      )
+    )
+    .catch(error => console.log("Error sending recovery mail: %o", error));
 });
 
 function showMessage(msg, el) {
