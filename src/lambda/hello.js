d99f7e46f84cb7cd980942809d5789db2d44e150
luna <lunaceee@gmail.com>
2018-05-04 13:32:15 -0700

    logging event and context in response body

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -6,9 +6,9 @@ const auth = new GoTrue({
 
 exports.handler = async function(event, context) {
   // your server-side functionality
-  console.log({ event, context });
+
   return {
     statusCode: 200,
-    body: "hello!"
+    body: { event, context };
   };
 };
