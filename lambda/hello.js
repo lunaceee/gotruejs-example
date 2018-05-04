061a055918d5142465f8260d265001106adf91de
luna <lunaceee@gmail.com>
2018-05-04 14:13:01 -0700

    wip

--- lambda/hello.js
+++ lambda/hello.js
@@ -251,8 +251,6 @@ const auth = new _gotrueJs2.default({
 
 exports.handler = (() => {
   var _ref = _asyncToGenerator(function* (event, context) {
-    const { identity, user } = context.clientContext;
-
     return {
       statusCode: 200,
       body: JSON.stringify([event, context.clientContext])
--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -5,8 +5,6 @@ const auth = new GoTrue({
 });
 
 exports.handler = async function(event, context) {
-  const { identity, user } = context.clientContext;
-
   return {
     statusCode: 200,
     body: JSON.stringify([event, context.clientContext])
