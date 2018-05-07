6f6f42ef1d636ccb239fb6976ac13dbe779632d9
luna <lunaceee@gmail.com>
2018-05-07 13:40:01 -0700

    changed url

--- lambda/hello.js
+++ lambda/hello.js
@@ -76,8 +76,8 @@ exports.handler = (() => {
   var _ref = _asyncToGenerator(function* (event, context) {
     const { identity, user } = context.clientContext;
     const userID = "3cb45f46-380c-44c3-ac53-33ff8696bf12";
-    const userUrl = "https://inspiring-ride-d3b2ae.netlify.com/.netlify/identity" + `admin/users/${userID}`;
-    console.log(userUrl);
+    const userUrl = `https://inspiring-ride-d3b2ae.netlify.com/.netlify/identity/admin/users/${userID}`;
+    console.log({ userUrl });
     return {
       statusCode: 200,
       body: JSON.stringify([event, userUrl, context.clientContext])
