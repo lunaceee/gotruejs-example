1a7fe3b43c78adfb81b7779a939edc4fc02d6009
luna <lunaceee@gmail.com>
2018-05-07 16:47:30 -0700

    debugging

--- lambda/hello.js
+++ lambda/hello.js
@@ -78,25 +78,27 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 exports.handler = function (event, context, callback) {
   const { identity, user } = context.clientContext;
-  const userID = "3cb45f46-380c-44c3-ac53-33ff8696bf12";
-  const userUrl = `https://inspiring-ride-d3b2ae.netlify.com/.netlify/identity/admin/users/${userID}`;
-  try {
-    (0, _nodeFetch2.default)(userUrl, { email: "luna+new@netlify.com", password: "gotrue" }).then(response => {
-      console.log("GOT HERE! 204!");
-      const newMeta = response.app_metadata;
-      console.log({ response });
-      callback(null, { statusCode: 204 });
-    }).catch(e => {
-      console.log("GOT HERE! 500! Internal.");
-      callback(null, {
-        statusCode: 500,
-        body: "Internal Server Error: " + e
-      });
-    });
-  } catch (e) {
-    console.log("GOT HERE! 500! outer");
-    callback(null, { statusCode: 500, body: "Internal Server Error: " + e });
-  }
+  console.log({ identity, user });
+  // const userUrl = `https://inspiring-ride-d3b2ae.netlify.com/.netlify/identity/admin/users/${userID}`;
+  // try {
+  //   fetch(userUrl, { email: "luna+new@netlify.com", password: "gotrue" })
+  //     .then(response => {
+  //       console.log("GOT HERE! 204!");
+  //       const newMeta = response.app_metadata;
+  //       console.log({ response });
+  //       callback(null, { statusCode: 204 });
+  //     })
+  //     .catch(e => {
+  //       console.log("GOT HERE! 500! Internal.");
+  //       callback(null, {
+  //         statusCode: 500,
+  //         body: "Internal Server Error: " + e
+  //       });
+  //     });
+  // } catch (e) {
+  //   console.log("GOT HERE! 500! outer");
+  //   callback(null, { statusCode: 500, body: "Internal Server Error: " + e });
+  // }
 }; // const fetch = require("node-fetch").default;
 
 /***/ }),
