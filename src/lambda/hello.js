e06050e7f324c081c55e53aaae86cb24de15bd91
luna <lunaceee@gmail.com>
2018-05-07 13:52:41 -0700

    removed console log

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -2,7 +2,6 @@ exports.handler = async function(event, context) {
   const { identity, user } = context.clientContext;
   const userID = "3cb45f46-380c-44c3-ac53-33ff8696bf12";
   const userUrl = `https://inspiring-ride-d3b2ae.netlify.com/.netlify/identity/admin/users/${userID}`;
-  console.log({ userUrl });
   console.log("url", context.clientContext.url);
   return {
     statusCode: 200,
